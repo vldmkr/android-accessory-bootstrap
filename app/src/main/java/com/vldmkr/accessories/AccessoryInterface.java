2068d849da070f2f607531dd212125923e178ebd
vldmkr <vladmakaryan@gmail.com>
2017-06-13 03:32:49 +0300

    adding javadoc

--- app/src/main/java/com/vldmkr/accessories/AccessoryInterface.java
+++ app/src/main/java/com/vldmkr/accessories/AccessoryInterface.java
@@ -1,5 +1,6 @@
 package com.vldmkr.accessories;
 
+import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -20,15 +21,41 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.Arrays;
+import java.util.Locale;
 
+/**
+ * This is an abstract class that implements the basic interaction with
+ * the USB device connected as USB host through Android Open Accessory (AOA) protocol.
+ *
+ * <p>Not all devices can support accessory mode. Suitable devices can be filtered
+ * using a <uses-feature> element in the AndroidManifest.
+ *
+ * <p>Communication with the application level code is carried out through the {@link Handler}.
+ * Posted item will be processed as soon as the message queue will ready to do so.
+ * Although this does not guarantee high speed of {@link Message} processing,
+ * but it satisfies the requirements.
+ *
+ * <p>The methods {@link #create} and {@link #destroy} must be called from the antagonistic callbacks
+ * of the application's life cycle, such as {@link Activity#onResume} and {@link Activity#onPause},
+ * respectively.
+ *
+ * <p>The methods
+ * {@link #getManufacturer},
+ * {@link #getModel},
+ * {@link #getVersion}
+ * are used to identify the USB accessory and must be implemented in the extended class.
+ *
+ * <p>The {@link #callback} method requires an override if the extended class does not use
+ * a communication {@link Handler}.
+ */
 public abstract class AccessoryInterface {
     private static final String TAG = AccessoryInterface.class.getSimpleName();
 
-    public static final int MSG_WHAT_ACCESSORY_ROW_DATA = -1;
-    public static final int MSG_WHAT_ACCESSORY_NOT_CONNECTED = -2;
-    public static final int MSG_WHAT_ACCESSORY_DETACHED = -3;
-    public static final int MSG_WHAT_ACCESSORY_PERMISSION_NOT_GRANTED = -4;
-    public static final int MSG_WHAT_ACCESSORY_INEQUALITY = -5;
+    public static final int MSG_WHAT_ACCESSORY_ROW_DATA = -1;               // obj is byte[]
+    public static final int MSG_WHAT_ACCESSORY_NOT_CONNECTED = -2;          // obj is null
+    public static final int MSG_WHAT_ACCESSORY_DETACHED = -3;               // obj is null
+    public static final int MSG_WHAT_ACCESSORY_PERMISSION_NOT_GRANTED = -4; // obj is null
+    public static final int MSG_WHAT_ACCESSORY_INEQUALITY = -5;             // obj is String
 
     private static final String ACTION_USB_PERMISSION = "com.AccessoryInterface.USB_PERMISSION";
     private boolean mIsPermissionRequestPending = false;
@@ -66,8 +93,13 @@ public abstract class AccessoryInterface {
         }
     };
 
+    /**
+     *
+     * @param communicationHandler
+     * @param bufferSize
+     */
     public AccessoryInterface(final Handler communicationHandler, final int bufferSize) {
-        HandlerThread workerHandlerThread = new HandlerThread("AccessoryInterface.WorkerHandlerThread");
+        final HandlerThread workerHandlerThread = new HandlerThread("AccessoryInterface.WorkerHandlerThread");
         workerHandlerThread.start();
         mWorkerHandler = new Handler(workerHandlerThread.getLooper(), new Handler.Callback() {
             @Override
@@ -80,6 +112,12 @@ public abstract class AccessoryInterface {
         mInBuffer = ByteBuffer.allocate(bufferSize);
     }
 
+    /**
+     * This method finds and opens an attached USB accessory if the caller has permission
+     * to access the accessory. If not, the corresponding request will be sent.
+     *
+     * @param context Context for which the {@link BroadcastReceiver} will be registered.
+     */
     public final void create(final Context context) {
         mUsbManager = (UsbManager) context.getSystemService(Context.USB_SERVICE);
         mPermissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), 0);
@@ -91,6 +129,11 @@ public abstract class AccessoryInterface {
         start();
     }
 
+    /**
+     * This method closes the connection with USB accessory if it is attached.
+     *
+     * @param context Context, which is accepted in the {@link #create} method.
+     */
     public final void destroy(final Context context) {
         context.unregisterReceiver(mUsbReceiver);
 
@@ -98,9 +141,11 @@ public abstract class AccessoryInterface {
     }
 
     protected void callback(Message msg) {
+        String logMsg = String.format(Locale.ENGLISH, "default callback; override it; message code: %d", msg.what);
         if (msg.what == MSG_WHAT_ACCESSORY_ROW_DATA) {
-            Log.w(TAG, String.format("default callback; override it; row data: %s", Arrays.toString((byte[]) msg.obj)));
+            logMsg = String.format(Locale.ENGLISH, "%s; row data: %s", logMsg, Arrays.toString((byte[]) msg.obj));
         }
+        Log.w(TAG, logMsg);
     }
 
     public abstract String getManufacturer();
@@ -112,6 +157,7 @@ public abstract class AccessoryInterface {
     private void start() {
         if (mFileDescriptor == null) {
             final UsbAccessory[] accessories = mUsbManager.getAccessoryList();
+            // in the current implementation of UsbManager there can be at most one attached USB accessory
             if (accessories != null && accessories[0] != null) {
                 final UsbAccessory accessory = accessories[0];
                 if (!getManufacturer().equals(accessory.getManufacturer())) {
